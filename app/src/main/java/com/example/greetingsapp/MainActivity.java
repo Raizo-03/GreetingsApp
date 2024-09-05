da3f464101fc8ad76cc0712bceed374e9b6f9fca
Raizo-03 <123054277+Raizo-03@users.noreply.github.com>
2024-09-05 09:25:19 +0800

    first application

---
+++ app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.greetingsapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        EditText edit = findViewById(R.id.editText);
+        Button startBtn = findViewById(R.id.buttonStart);
+        TextView title = findViewById(R.id.title);
+
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edit.getText().toString();
+
+               Toast.makeText(MainActivity.this, "Hello " + name, Toast.LENGTH_SHORT).show();
+
+
+
+             }
+          });
+
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                    return insets;
+                });
+    }
+}
